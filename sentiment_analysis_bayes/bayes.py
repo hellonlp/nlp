588f61a69b77562cb425b9ccacc65882c6ed0d75
chen ming <38547392+hellonlp@users.noreply.github.com>
2018-11-16 11:07:45 +0800

    Update bayes.py

--- sentiment_analysis_bayes/bayes.py
+++ sentiment_analysis_bayes/bayes.py
@@ -23,21 +23,20 @@ def classify(vec2Classify,p0Vec,p1Vec,pClass1):
 #加载模型
 def load_p0Vec_p1Vec_pClass1():
     f = os.path.join(pwd,'parametre_pearson_40000','p0Vec.txt')
-    fp=open(f,encoding='utf-8');lines=fp.readlines();fp.close()
-    p0Vec=[]
-    for i  in lines: 
-        p0Vec.append(float(i))
+    with open(f,encoding='utf-8') as fp:
+        lines = fp.readlines()
+    p0Vec = [float(l) for l in lines]
     p0Vec = np.array(p0Vec)
     #
     f = os.path.join(pwd,'parametre_pearson_40000','p1Vec.txt')
-    fp=open(f,encoding='utf-8');lines=fp.readlines();fp.close()
-    p1Vec=[]
-    for i  in lines: 
-        p1Vec.append(float(i))
+    with open(f,encoding='utf-8') as fp:
+        lines = fp.readlines()
+    p1Vec=[float(l) for l in lines]]
     p1Vec = np.array(p1Vec)
     #
     f = os.path.join(pwd,'parametre_pearson_40000','pClass1.txt')
-    fp=open(f,encoding='utf-8'); lines=fp.readlines();fp.close()
+    with open(f,encoding='utf-8') as fp:
+         lines=fp.readlines()
     pClass1 = float(lines[0])
     return p0Vec,p1Vec,pClass1
 
@@ -45,17 +44,14 @@ def load_p0Vec_p1Vec_pClass1():
 
 # 读取停用词(或者标点符号)
 f = os.path.join(pwd,'dict','ponctuation_sentiment.txt')
-fp = open(f,encoding='utf-8')
-lines = fp.readlines()
-fp.close()
-stopwords=[line.strip('\n')  for line in lines]
+with open(f,encoding='utf-8') as fp:
+    lines = fp.readlines()
+stopwords = set([line.strip('\n')  for line in lines])
   
   
 # 去除停用词 ok
 def drop_stopwords(sentence):
     segResult = jieba.lcut(sentence)
-
-
     newSent = []
     for word in segResult:
         if word in stopwords:
@@ -67,9 +63,8 @@ def drop_stopwords(sentence):
 
 #读取词汇特征
 f = os.path.join(pwd,'data','vocabulary_pearson_40000.txt')
-fp = open(f,encoding='utf8')
-vocabulary = fp.readlines()
-fp.close()
+with open(f,encoding='utf8') as fp:
+    vocabulary = fp.readlines()
 vocabulary = [texte.replace('\n','') for texte in vocabulary ]
 def set_vector(sentence):
     line = drop_stopwords(sentence)
